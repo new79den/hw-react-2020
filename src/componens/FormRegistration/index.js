d1f373a3f5b38b3e57a2ee45bac51f6fb24a58c4
denis.korniychuk <new79den@gmail.com>
2020-08-21 17:03:04 +0300

    homework 5,4 done

--- src/componens/FormRegistration/index.js
+++ src/componens/FormRegistration/index.js
@@ -23,6 +23,7 @@ export const FormRegistration = () => {
     const history = useHistory();
 
     const submitForm = (values) => {
+        console.log('submit');
         setLocalStorageData(values);
         setFormState(values);
         history.push(book.student);
@@ -30,27 +31,75 @@ export const FormRegistration = () => {
     }
 
 
+    const changeField = (value) => {
+        let error = false
+        if (!value) {
+            error = 'Required';
+        } else if (value.length < 2) {
+            error = 'Короткое имя';
+        }
+        return error;
+    }
+
+    const validate = (values) => {
+        const errors = {};
+
+        if (!values.email) {
+            errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+        }
+
+        if ( changeField(values.firstName) ){
+            errors.surname = changeField(values.firstName);
+        }
+
+        if ( changeField(values.surname) ){
+            errors.surname = changeField(values.surname);
+        }
+
+        if (values.age < 6){
+            errors.age = 'Должно быть больше 6'
+        } else if(errors.age > 60){
+            errors.age = 'Должно меньше больше 69'
+        }
+
+        return errors;
+    };
+
+
     return (
         <section className={style.wrap}>
             <Formik
                 initialValues={formStage}
                 onSubmit={submitForm}
+                validate = {validate}
             >
 
-                {({values}) => {
+                {({values, errors, touched}) => {
+                    console.log(errors);
                     return (
                         <Form>
-                            <label htmlFor="sex">first name</label>
+                            <label htmlFor="firstName">first name</label>
                             <Field type="text" name="firstName"/>
+                            {errors.firstName && touched.firstName ? <div className='error'>{errors.firstName}</div> : null}
+                            <br/>
 
-                            <label htmlFor="sex">surname</label>
+                            <label htmlFor="surname">surname</label>
                             <Field type="text" name="surname"/>
+                            {errors.surname && touched.surname ? <div className='error'>{errors.surname}</div> : null}
+                            <br/>
 
-                            <label htmlFor="sex">age</label>
+                            <label htmlFor="age">age</label>
                             <Field type="number" name="age"/>
+                            {errors.age && touched.age ? <div className='error'>{errors.age}</div> : null}
+                            <br/>
 
-                            <label htmlFor="sex">email</label>
+                            <label htmlFor="email">email</label>
                             <Field type="email" name="email"/>
+                            {errors.email && touched.email ? <div className='error'>{errors.email}</div> : null}
+                            <br/>
+
 
                             <div>
                                 <label htmlFor="sex">male</label>
@@ -61,7 +110,7 @@ export const FormRegistration = () => {
                                     value="male"
                                 />
 
-                                <label htmlFor="sex">female</label>
+                                <label htmlFor="female">female</label>
                                 <Field
                                     checked={values.sex === 'female'}
                                     type="radio"
